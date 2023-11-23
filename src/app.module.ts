b13521221525c8ac271c087c29675c114cf77941
Jorge L. Mejia Pejerrey <mejiapj@gmail.com>
2023-11-22 22:45:22 -0500

    Clase 6/24 -> Instalando y conectando MongoDB Driver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
-import { Module, HttpModule, HttpService } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
-
+// import { MongoClient } from 'mongodb';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -11,7 +12,26 @@ import { DatabaseModule } from './database/database.module';
 import { enviroments } from './enviroments';
 import config from './config';
 
+import { MongoClient } from 'mongodb';
 
+const uri =
+  'mongodb://root:root@localhost:27017/?authSource=admin&readPreference=primary';
+
+async function run() {
+  const client = await MongoClient.connect(uri);
+
+  try {
+    const database = client.db('platzi-store');
+    const taskCollection = database.collection('tasks');
+    const tasks = await taskCollection.find().toArray();
+    console.log(tasks);
+  } finally {
+    // Asegúrate de cerrar la conexión cuando hayas terminado
+    await client.close();
+  }
+}
+
+run().catch(console.error);
 
 @Module({
   imports: [
